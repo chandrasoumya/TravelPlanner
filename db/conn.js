1d7765a49ccd9aa9ab74b17184f7cc9c6e5337cc
chandrasoumya <121810090+chandrasoumya@users.noreply.github.com>
2024-03-31 06:53:18 +0000

    timeout  error fix

--- db/conn.js
+++ db/conn.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://soumya220033:chandra2003%40@tplanner.g6ciukq.mongodb.net/?retryWrites=true&w=majority&appName=TPlanner", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Increase this value
+    socketTimeoutMS: 45000, // Increase this value
 }).then(() => {
     console.log("Database is connected");
 }).catch((err) => {
